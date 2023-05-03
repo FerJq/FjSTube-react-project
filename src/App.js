747f22ead2bb8a9661a5a742b1755f0c83d3eb7e
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-05-02 22:53:49 -0400

    feed export fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import {
-  ChannelDetail,
-  Feed,
   Navbar,
+  Feed,
   VideoDetail,
+  ChannelDetail,
   SearchFeed,
-  SearchBar,
 } from "./components";
 
 const App = () => (
@@ -14,7 +13,7 @@ const App = () => (
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route exact path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
