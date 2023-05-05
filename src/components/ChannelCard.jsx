6103cb5cac4572a073e0b312edd3111d674b9200
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-05-04 21:52:50 -0400

    videocard layout

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { demoProfilePicture } from "../utils/constants";
+import { Link } from "react-router-dom";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
-};
+const ChannelCard = ({ channelDetail }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+    }}
+  >
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      >
+        <CardMedia
+          image={
+            channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
+          }
+          alt={channelDetail?.snippet?.title}
+          sx={{
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
+            border: "1px solid #e3e3e3",
+            mb: 2,
+          }}
+        />
+        <Typography varient="h6">
+          {channelDetail?.snippet?.title}
+          <CheckCircleIcon
+            sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
+          />
+        </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography>
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount
+            ).toLocaleString()}{" "}
+            Subscribers
+          </Typography>
+        )}
+      </CardContent>
+    </Link>
+  </Box>
+);
 
 export default ChannelCard;
