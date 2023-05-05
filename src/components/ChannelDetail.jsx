8cb85fa62f160a9a4c827ebc4e01c10a22ad7f7b
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-05-04 22:32:48 -0400

    useParams from react-router-dom to get data from the API to channelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const { id } = useParams();
+  const [channel, setChannel] = useState(null);
+  console.log(channel);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part"=snippet&id=${id}`).then((data) =>
+      setChannel(data?.items[0])
+    );
+  }, [id]);
+
+  return <Box></Box>;
 };
 
 export default ChannelDetail;
